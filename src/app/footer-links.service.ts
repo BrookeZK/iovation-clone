183bbd6a9f1c4b339e01c7c126d7b2c5764f43ca
Brooke Kullberg <brookekullberg@gmail.com>
2019-06-21 14:07:51 -0700

    update footerlinks.service to grab data from firebase

--- src/app/footer-links.service.ts
+++ src/app/footer-links.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class FooterLinksService {
-
-  constructor() { }
+  links: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) {
+    this.links = database.list('footer-links');
+   }
 
   getLinks() {
-    // return ;
+    return this.links;
   }
 }
