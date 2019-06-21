dbba946cc7fe7ebff8726d079a251c228390d90c
Brooke Kullberg <brookekullberg@gmail.com>
2019-06-21 15:44:50 -0700

    add third article component

---
+++ src/app/third-article/third-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-third-article',
+  templateUrl: './third-article.component.html',
+  styleUrls: ['./third-article.component.css']
+})
+export class ThirdArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
