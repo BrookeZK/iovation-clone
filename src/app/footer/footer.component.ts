a7102a9f926c35e29eee75d5711b35aa2d8f0f9c
Brooke Kullberg <brookekullberg@gmail.com>
2019-06-21 14:05:40 -0700

    add code to gather links from database - not fully operational

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FooterLinksService } from '../footer-links.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  providers: [FooterLinksService]
 })
 export class FooterComponent implements OnInit {
+  // links: []
 
-  constructor() { }
+  constructor(private footerLinkService: FooterLinksService) { }
 
   ngOnInit() {
+    // this.links = this.footerLinkService.getLinks();
   }
 
 }
